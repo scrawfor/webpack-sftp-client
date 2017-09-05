8136aa1c89b36a4c22eedd6ffa1ef316814a2a0b
Steven Crawford <Steven_Crawford@PremierInc.com>
2017-09-05 08:56:44 -0400

    Actual fixed version!

--- index.js
+++ index.js
@@ -1,92 +1,37 @@
-/*!
- *  <https://github.com/sqhtiamo/webpack-sftp-client.git>
- *
- * Copyright (c) 2016-2017, Cloughzhang(Zhang Yuhang).
- * Licensed under the MIT License.
- */
-
-'use strict';
-
 var ClientLib = require('scp2');
 
-var client = new ClientLib.Client();
-
 function WebpackSftpClient(options) {
-    this.options = options;
+  this.options = options;
 }
 
 WebpackSftpClient.prototype.apply = function (compiler) {
 
-    var self = this;
+  var self = this;
 
-    compiler.plugin('after-emit', function (compilation) {
+  compiler.plugin('after-emit', function (compilation) {
 
-        var remotePath = self.options.remotePath;
-        var path = self.options.path;
-        var username = self.options.username;
-        var host = self.options.host;
-        var password = self.options.password;
-        var port = self.options.port || '22';
-        var verbose = self.options.verbose;
+    var remotePath = self.options.remotePath;
+    var path = self.options.path;
+    var username = self.options.username;
+    var host = self.options.host;
+    var password = self.options.password;
+    var port = self.options.port || '22';
+    var verbose = self.options.verbose;
 
-        var startTime;
-        var endTime;
-
-        client.on('connect', function () {
-            // console.log('connected');
-        });
-
-        client.on('ready', function () {
-            // console.log('ready');
-            startTime = new Date();
-            console.log('[Start Uploading] ' + startTime);
-        });
-
-        client.on('transfer', function (buf, up, total) {
-        });
-
-        client.on('write', function (p) {
-            if (verbose) {
-                console.log('Transfer ' + p.source + ' => ' + p.destination);
-            }
-        });
-
-        client.on('end', function () {
-            endTime = new Date();
-            console.log('[End Uploading] ' + new Date());
-        });
-
-        client.on('error', function (err) {
-            console.log('[Error] ' + err);
-        });
-
-        client.on('close', function () {
-            console.log('Transfer with SFTP Completed in [' + (+endTime - +startTime) / 1000 + '] seconds!');
-        });
-
-
-        var srcPath = path;
-        var destPath = username + ':' + password + '@' + host + ':' + port + ':' + remotePath;
-
-        uploadByDir(srcPath, destPath, client);
-
-    });
-};
-
-/**
- * [uploadByDir: Upload Directory Directory & Cannot Get Detailed Uploading Info for Files]
- * @param  {[String]} src    [description]
- * @param  {[String]} dest   [description]
- * @param  {[Object]} client [description]
- */
-function uploadByDir(src, dest, client) {
-    ClientLib.scp(src, dest, client,
-        function (err) {
-            if (err) {
-                console.log(err);
-            }
+    var srcPath = path;
+    var destPath = username + ':' + password + '@' + host + ':' + port + ':' + remotePath;
+    
+    ClientLib.scp(srcPath, destPath,
+      function (err) {
+        
+        if (err) {
+          console.log('FTP Upload Error: ', err);
+        } else {
+          console.log('Uploaded File: ' + srcPath,'->',destPath);
         }
+      }
     );
-}
+  });
+};
 
 module.exports = WebpackSftpClient;
